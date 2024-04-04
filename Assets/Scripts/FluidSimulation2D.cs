f1a97b2f852bac18ebccdf459c773f719b17a70e
monsterlady <xuruikang6@hotmail.com>
2024-04-04 19:56:30 +0200

    feat: final upload

---
+++ Assets/Scripts/FluidSimulation2D.cs
@@ -0,0 +1,225 @@
+using UnityEngine;
+using Unity.Mathematics;
+
+public class FluidSimulation2D : MonoBehaviour
+{
+    public event System.Action OnSimulationStepComplete;
+
+    [Header("Simulation Parameters")]
+    public float simulationSpeed = 1;
+    public bool useFixedTimeSteps;
+    public int stepsPerFrame;
+    public float simulationGravity;
+    [Range(0, 1)] public float restitutionCoefficient = 0.95f;
+    public float interactionRadius = 2;
+    public float fluidDensityTarget;
+    public float pressureFactor;
+    public float nearbyPressureFactor;
+    public float viscosityCoefficient;
+    public Vector2 simulationBounds;
+    public Vector2 obstacleDimensions;
+    public Vector2 obstaclePosition;
+
+    [Header("Interaction Parameters")]
+    public float userInteractionRadius;
+    public float userForceMagnitude;
+
+    [Header("Shader References")]
+    public ComputeShader simulationComputeShader;
+    public ParticleSpawner particleGenerator;
+    public ParticleRenderer particleRenderer;
+
+    // GPU Buffers
+    public ComputeBuffer particlePositionsBuffer { get; private set; }
+    public ComputeBuffer particleVelocitiesBuffer { get; private set; }
+    public ComputeBuffer fluidDensityBuffer { get; private set; }
+    ComputeBuffer predictedPositionsBuffer;
+    ComputeBuffer hashGridIndices;
+    ComputeBuffer hashGridOffsets;
+    GPUSort sortingAlgorithm;
+
+    // Compute Shader Kernel Indices
+    const int applyExternalForcesKernel = 0;
+    const int computeSpatialHashKernel = 1;
+    const int computeDensityKernel = 2;
+    const int computePressureKernel = 3;
+    const int computeViscosityKernel = 4;
+    const int updateParticlePositionsKernel = 5;
+
+    // Simulation State
+    bool simulationPaused;
+    ParticleSpawner.ParticleSpawnData initialSpawnData;
+    bool pauseAfterNextFrame;
+
+    public int totalParticles { get; private set; }
+
+    // 在游戏开始时初始化
+    void Start()
+    {
+        Debug.Log("开始初始化FluidSimulation2D");
+        float frameDeltaTime = 1 / 60f;
+        Time.fixedDeltaTime = frameDeltaTime;
+
+        initialSpawnData = particleGenerator.GetSpawnData();
+        totalParticles = initialSpawnData.positions.Length;
+
+        // 初始化GPU缓存
+        particlePositionsBuffer = ComputeHelper.CreateStructuredBuffer<float2>(totalParticles);
+        predictedPositionsBuffer = ComputeHelper.CreateStructuredBuffer<float2>(totalParticles);
+        particleVelocitiesBuffer = ComputeHelper.CreateStructuredBuffer<float2>(totalParticles);
+        fluidDensityBuffer = ComputeHelper.CreateStructuredBuffer<float2>(totalParticles);
+        hashGridIndices = ComputeHelper.CreateStructuredBuffer<uint3>(totalParticles);
+        hashGridOffsets = ComputeHelper.CreateStructuredBuffer<uint>(totalParticles);
+
+        // 设置缓存数据
+        InitializeBufferWithData(initialSpawnData);
+
+        // 设置计算着色器
+        ComputeHelper.SetBuffer(simulationComputeShader, particlePositionsBuffer, "Positions", applyExternalForcesKernel, updateParticlePositionsKernel);
+        ComputeHelper.SetBuffer(simulationComputeShader, predictedPositionsBuffer, "PredictedPositions", applyExternalForcesKernel, computeSpatialHashKernel, computeDensityKernel, computePressureKernel, computeViscosityKernel);
+        ComputeHelper.SetBuffer(simulationComputeShader, hashGridIndices, "SpatialIndices", computeSpatialHashKernel, computeDensityKernel, computePressureKernel, computeViscosityKernel);
+        ComputeHelper.SetBuffer(simulationComputeShader, hashGridOffsets, "SpatialOffsets", computeSpatialHashKernel, computeDensityKernel, computePressureKernel, computeViscosityKernel);
+        ComputeHelper.SetBuffer(simulationComputeShader, fluidDensityBuffer, "Densities", computeDensityKernel, computePressureKernel, computeViscosityKernel);
+        ComputeHelper.SetBuffer(simulationComputeShader, particleVelocitiesBuffer, "Velocities", applyExternalForcesKernel, computePressureKernel, computeViscosityKernel, updateParticlePositionsKernel);
+
+        simulationComputeShader.SetInt("numParticles", totalParticles);
+
+        sortingAlgorithm = new GPUSort();
+        sortingAlgorithm.SetBuffers(hashGridIndices, hashGridOffsets);
+
+        // 初始化粒子显示
+        particleRenderer.Init(this);
+    }
+
+    // 在每个固定的物理更新帧调用
+    void FixedUpdate()
+    {
+        if (useFixedTimeSteps)
+        {
+            ExecuteSimulationFrame(Time.fixedDeltaTime);
+        }
+    }
+
+    // 在每个更新帧调用
+    void Update()
+    {
+        if (!useFixedTimeSteps && Time.frameCount > 10)
+        {
+            ExecuteSimulationFrame(Time.deltaTime);
+        }
+
+        if (pauseAfterNextFrame)
+        {
+            simulationPaused = true;
+            pauseAfterNextFrame = false;
+        }
+
+        ProcessUserInput();
+    }
+
+    // 执行模拟帧，分解为多个模拟步骤
+    void ExecuteSimulationFrame(float frameDuration)
+    {
+        if (!simulationPaused)
+        {
+            float stepDuration = frameDuration / stepsPerFrame * simulationSpeed;
+
+            ApplySimulationSettings(stepDuration);
+
+            for (int i = 0; i < stepsPerFrame; i++)
+            {
+                PerformSimulationStep();
+                OnSimulationStepComplete?.Invoke();
+            }
+        }
+    }
+
+    // 执行单个模拟步骤
+    void PerformSimulationStep()
+    {
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: applyExternalForcesKernel);
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: computeSpatialHashKernel);
+        sortingAlgorithm.SortAndCalculateOffsets();
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: computeDensityKernel);
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: computePressureKernel);
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: computeViscosityKernel);
+        ComputeHelper.Dispatch(simulationComputeShader, totalParticles, kernelIndex: updateParticlePositionsKernel);
+    }
+
+    // 应用模拟设置
+    void ApplySimulationSettings(float deltaTime)
+    {
+        simulationComputeShader.SetFloat("deltaTime", deltaTime);
+        simulationComputeShader.SetFloat("gravity", simulationGravity);
+        simulationComputeShader.SetFloat("collisionDamping", restitutionCoefficient);
+        simulationComputeShader.SetFloat("smoothingRadius", interactionRadius);
+        simulationComputeShader.SetFloat("targetDensity", fluidDensityTarget);
+        simulationComputeShader.SetFloat("pressureMultiplier", pressureFactor);
+        simulationComputeShader.SetFloat("nearPressureMultiplier", nearbyPressureFactor);
+        simulationComputeShader.SetFloat("viscosityStrength", viscosityCoefficient);
+        simulationComputeShader.SetVector("boundsSize", simulationBounds);
+        simulationComputeShader.SetVector("obstacleSize", obstacleDimensions);
+        simulationComputeShader.SetVector("obstacleCentre", obstaclePosition);
+
+        // 根据鼠标输入计算交互力
+        Vector2 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bool impluse = Input.GetMouseButton(0);
+        float interactionStrength = 0;
+        if (impluse)
+        {
+            Debug.Log("鼠标左键按下: " + currentMousePosition + " " + userForceMagnitude);
+            interactionStrength = -userForceMagnitude;
+        }
+
+        simulationComputeShader.SetVector("interactionInputPoint", currentMousePosition);
+        simulationComputeShader.SetFloat("interactionInputStrength", interactionStrength);
+        simulationComputeShader.SetFloat("interactionInputRadius", userInteractionRadius);
+    }
+
+    // 用初始数据初始化缓存
+    void InitializeBufferWithData(ParticleSpawner.ParticleSpawnData spawnInfo)
+    {
+        float2[] positions = new float2[spawnInfo.positions.Length];
+        System.Array.Copy(spawnInfo.positions, positions, spawnInfo.positions.Length);
+
+        particlePositionsBuffer.SetData(positions);
+        predictedPositionsBuffer.SetData(positions);
+        particleVelocitiesBuffer.SetData(spawnInfo.velocities);
+    }
+
+    // 处理用户输入
+    void ProcessUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            simulationPaused = !simulationPaused;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            simulationPaused = false;
+            pauseAfterNextFrame = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            simulationPaused = true;
+            InitializeBufferWithData(initialSpawnData);
+            PerformSimulationStep();
+            InitializeBufferWithData(initialSpawnData);
+        }
+    }
+
+    // 在对象销毁时释放资源
+    void OnDestroy()
+    {
+        ComputeHelper.Release(particlePositionsBuffer, predictedPositionsBuffer, particleVelocitiesBuffer, fluidDensityBuffer, hashGridIndices, hashGridOffsets);
+    }
+
+    // 在场景中绘制辅助图形
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.4f);
+        Gizmos.DrawWireCube(Vector2.zero, simulationBounds);
+        Gizmos.DrawWireCube(obstaclePosition, obstacleDimensions);
+    }
+}
---
+++ Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using Unity.Mathematics;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    public int count; // 粒子数量
+
+    public Vector2 initialSpeed; // 初始速度
+    public Vector2 center; // 生成中心点
+    public Vector2 size; // 生成区域大小
+    public float jitterIntensity; // 抖动强度
+    public bool displaySpawnArea; // 是否显示生成区域的Gizmo
+
+    // 获取生成数据
+    public ParticleSpawnData GetSpawnData()
+    {
+        ParticleSpawnData data = new ParticleSpawnData(count);
+        var randomGenerator = new Unity.Mathematics.Random(42); // 随机数生成器
+
+        for (int i = 0; i < count; i++)
+        {
+            // 生成在指定区域内的随机位置
+            float2 position = new float2(
+                center.x + (randomGenerator.NextFloat() - 0.5f) * size.x,
+                center.y + (randomGenerator.NextFloat() - 0.5f) * size.y
+            );
+
+            // 计算抖动，用于在初始速度上添加随机变化
+            float angle = randomGenerator.NextFloat() * Mathf.PI * 2;
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            Vector2 jitter = direction * jitterIntensity * (randomGenerator.NextFloat() - 0.5f);
+
+            // 设置粒子的位置和速度（加上抖动）
+            data.positions[i] = position;
+            data.velocities[i] = initialSpeed + jitter;
+        }
+
+        return data;
+    }
+
+    // 粒子生成数据结构
+    public struct ParticleSpawnData
+    {
+        public float2[] positions; // 位置数组
+        public float2[] velocities; // 速度数组
+
+        public ParticleSpawnData(int num)
+        {
+            positions = new float2[num];
+            velocities = new float2[num];
+        }
+    }
+
+    // 在编辑器中绘制生成区域
+    void OnDrawGizmos()
+    {
+        if (displaySpawnArea && !Application.isPlaying)
+        {
+            Gizmos.color = new Color(1, 1, 0, 0.5f);
+            Gizmos.DrawWireCube(center, size);
+        }
+    }
+}
